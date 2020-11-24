9d0446bc95df594a2599a95084066b7e4eb9944c
danielVFS <danielv759153@gmail.com>
2020-11-24 15:18:22 -0300

    feat: dashboard components (Styles)

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,3 +1,18 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 250px auto; //2 Columns
+  grid-template-rows: 70px auto; //2 Rows
+
+  /*
+    * AS: Aside
+    * MH: MainHeader
+    * CT: Content
+  */
+  grid-template-areas:
+    "AS MH"
+    "AS CT";
+
+  height: 100vh;
+`;
