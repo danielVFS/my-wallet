5864c1a32c3dbe68816322b6b574427b8d5a5609
danielVFS <danielv759153@gmail.com>
2020-11-24 15:51:53 -0300

    feat: theme swticher working

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+
+import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 
 import Layout from "./components/Layout";
+import dark from "./styles/themes/dark";
+import light from "./styles/themes/light";
 
 const App: React.FC = () => {
   return (
-    <React.Fragment>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <Layout />
-    </React.Fragment>
+    </ThemeProvider>
   );
 };
 
