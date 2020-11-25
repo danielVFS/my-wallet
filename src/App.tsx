d9a9892034ce2b86a3236785b6c8a8eef9675657
danielVFS <danielv759153@gmail.com>
2020-11-25 14:19:23 -0300

    feat: ContentHeader title

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 
 import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
 import dark from "./styles/themes/dark";
 import light from "./styles/themes/light";
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={dark}>
+    <ThemeProvider theme={light}>
       <GlobalStyles />
-      <Layout />
+      <Layout>
+        <Dashboard />
+      </Layout>
     </ThemeProvider>
   );
 };
