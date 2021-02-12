5c417642a0cc4c6d9094e7269327f8dc1668bbec
danielVFS <danielv759153@gmail.com>
2021-02-12 18:30:55 -0300

    getting image with dropzone

--- src/components/Dropzone/index.tsx
+++ src/components/Dropzone/index.tsx
@@ -1,21 +1,33 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { FaPlusCircle } from 'react-icons/fa'
 import { useDropzone } from 'react-dropzone';
 
 import './styles.css';
+import { url } from 'inspector';
 
 const Dropzone = () => {
+  const [selectedFileUrl, setSelectedFileUrl] = useState('');
+  
   const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles);
+    const file = acceptedFiles[0]; // um único arquivo
+
+    const fileUrl = URL.createObjectURL(file);
+
+    setSelectedFileUrl(fileUrl);
   }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
 
   return (
     <div className="dropzone" {...getRootProps()}>
       <input {...getInputProps()} />
-      <p>
-        <FaPlusCircle />
-      </p>
+      { selectedFileUrl
+        ? 
+        <img src={selectedFileUrl} alt="Imagem da Moeda"/>
+        :
+        <p>
+          <FaPlusCircle />
+        </p>
+      }
     </div>
   )
 }
