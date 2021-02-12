1909942dc4f56d6fa19f666ba75f46e0f9a8f722
danielVFS <danielv759153@gmail.com>
2021-02-12 16:44:28 -0300

    some itens in form with bootstrap

--- src/pages/CreateCoin/index.tsx
+++ src/pages/CreateCoin/index.tsx
@@ -3,7 +3,7 @@ import { Form, Button, Col } from 'react-bootstrap';
 
 import ContentHeader from "../../components/ContentHeader";
 
-import { Container, Content } from "./styles";
+import { Container, Content, CirculationContainer, CirculationTitle, CirculationOptions } from "./styles";
 
 const CreateCoin: React.FC = () => {
   return (
@@ -13,54 +13,109 @@ const CreateCoin: React.FC = () => {
       <Content>
         <Form>
           <Form.Row>
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+            <Form.Group as={Col} controlId="formTitle">
+              <Form.Label>Título</Form.Label>
+              <Form.Control type="text" placeholder="200 reais" />
             </Form.Group>
           </Form.Row>
-
-          <Form.Group controlId="formGridAddress1">
-            <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="1234 Main St" />
-          </Form.Group>
-
-          <Form.Group controlId="formGridAddress2">
-            <Form.Label>Address 2</Form.Label>
-            <Form.Control placeholder="Apartment, studio, or floor" />
-          </Form.Group>
-
+          
           <Form.Row>
-            <Form.Group as={Col} controlId="formGridCity">
-              <Form.Label>City</Form.Label>
-              <Form.Control />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Label>State</Form.Label>
-              <Form.Control as="select" defaultValue="Choose...">
-                <option>Choose...</option>
-                <option>...</option>
+            <Form.Group as={Col} controlId="formType">
+              <Form.Label>Tipo</Form.Label>
+              <Form.Control as="select" defaultValue="Cédula">
+                <option>Cédula</option>
+                <option>Moeda</option>
               </Form.Control>
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridZip">
-              <Form.Label>Zip</Form.Label>
-              <Form.Control />
+            <Form.Group as={Col} controlId="formClass">
+              <Form.Label>Classe</Form.Label>
+              <Form.Control as="select" defaultValue="Brasil República 1942-2055">
+                <option>Brasil-República 1942-2055</option>
+                <option>Brasil-Democrata 1969-2099</option>
+              </Form.Control>
+            </Form.Group>
+
+            <CirculationContainer>
+              <CirculationTitle>Período de Circulação</CirculationTitle>
+
+              <CirculationOptions>
+                <Form.Group as={Col} controlId="formTitle">
+                  <Form.Control type="text" placeholder="01/07/1969" />
+                </Form.Group>
+                <Form.Group as={Col} controlId="formTitle">
+                  <Form.Control type="text" placeholder="até hoje" />
+                </Form.Group>
+              </CirculationOptions>
+            </CirculationContainer>
+
+            <Form.Group as={Col} controlId="formMonetaryStandard">
+              <Form.Label>Padrão Monetário</Form.Label>
+              <Form.Control as="select" defaultValue="Real 1994-1998">
+                <option>Real 1994-1998</option>
+                <option>Real2 1995-1996</option>
+              </Form.Control>
             </Form.Group>
           </Form.Row>
 
-          <Form.Group id="formGridCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
+          <Form.Row>
+            <Form.Group as={Col} controlId="formPrint">
+              <Form.Label>Estampa</Form.Label>
+              <Form.Control type="text" placeholder="Segunda" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formEmitter">
+              <Form.Label>Emissor</Form.Label>
+              <Form.Control type="text" placeholder="Brasil" defaultValue="Brasil"/>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formManufacter">
+              <Form.Label>Fabricante</Form.Label>
+              <Form.Control type="text"/>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formValue">
+              <Form.Label>Valor</Form.Label>
+              <Form.Control type="text" placeholder="200" />
+            </Form.Group>
+          </Form.Row>
+          
+          <div className="d-flex justify-content-center">
+            <p className="h4">Especificações Físicas</p>
+          </div>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formHeight">
+              <Form.Label>Altura</Form.Label>
+              <Form.Control type="text" placeholder="6.5cm" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formWidth">
+              <Form.Label>Largura</Form.Label>
+              <Form.Control type="text" placeholder="14.2cm"/>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formManufacter">
+              <Form.Label>Espessura</Form.Label>
+              <Form.Control type="text"/>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formMaterial">
+              <Form.Label>Material</Form.Label>
+              <Form.Control type="text" placeholder="Papel fiduciário" />
+            </Form.Group>
+          </Form.Row>
+          
+          <Form.Group controlId="formDescription">
+            <Form.Label>Descrição</Form.Label>
+            <Form.Control as="textarea" rows={3} />
           </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
+          <div className="d-flex justify-content-center">
+            <Button variant="danger" type="submit">
+              Salvar
+            </Button>
+          </div>
         </Form>
       </Content>
     </Container>
--- src/pages/CreateCoin/styles.ts
+++ src/pages/CreateCoin/styles.ts
@@ -4,3 +4,18 @@ export const Container = styled.div``;
 
 export const Content = styled.main``;
 
+export const CirculationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 7px;
+`;
+
+export const CirculationTitle = styled.div``;
+
+export const CirculationOptions = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
